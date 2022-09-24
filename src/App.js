cbae77fa5aa34367fb1cfd8e2194915972073558
Andreja Nesic <andreja.nesic@brerasolutions.com>
2022-09-24 17:25:31 +0200

    added TodoList and TodoManager

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+import TodoList from "./TodoList";
+import TodoManager from "./TodoManager";
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
   return (
     <div className="App">
+      <TodoList />
+      <TodoManager />
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+function TodoList() {
+  return (
+    <div className="TodoList">
+    </div>
+  );
+}
+
+export default TodoList;
